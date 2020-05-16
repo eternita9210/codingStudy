ab773b645abdfb73216f08775f967c8e6abdf563
eternita9210 <58546141+eternita9210@users.noreply.github.com>
2020-05-17 00:35:48 +0900

    first : clock, name

---
+++ 16_May_2020_Study_JS/js/clock.js
@@ -0,0 +1,26 @@
+// 페이지 중앙 시계 기능
+const clockContainer = document.querySelector(".clock"),
+    clockText = clockContainer.querySelector(".clock_text");
+
+function getTime() {
+    const time = new Date();
+    const hours = time.getHours();
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+    // 백틱 : `` >  ${}를 사용해서 변수명과 문자열을 결합할 때 사용
+    const now = 
+    `${hours < 10 ? `0${hours}` : hours}:
+    ${minutes < 10 ? `0${minutes}` : minutes}:
+    ${seconds < 10 ? `0${seconds}` : seconds}`;
+
+    clockContainer.innerHTML = now;
+}
+
+function init(){
+    // 시간 알아내기
+        getTime();
+        setInterval(getTime, 1000);
+    
+}
+
+init();
---
+++ 16_May_2020_Study_JS/js/hello.js
@@ -0,0 +1,54 @@
+const nameContainer = document.querySelector(".myName");
+
+function drawName(name) {
+    nameContainer.innerHTML = "";
+    const drawName = document.createElement("span");
+    drawName.className = "name_text";
+    drawName.innerHTML = `Hello! ${name}!`;
+    nameContainer.appendChild(drawName);
+}
+
+function handleSubmit(event) {
+    event.preventDefault(); //이벤트 버블링 막기
+    const form = event.target;
+    const input = form.querySelector("input");
+    const name = input.value; // input 입력값의 value 받아오기
+    localStorage.setItem("username", name);
+    drawName(name);
+}
+
+function drawInput() {
+    // 이름 입력창
+    const input = document.createElement("input");
+    input.type = "text";
+    input.class = "input_name";
+    input.placeholder = "Type your name here!";
+    
+    const form = document.createElement("form");
+
+    form.addEventListener("submit", handleSubmit);
+    form.appendChild(input);
+    nameContainer.appendChild(form);
+}
+
+function checkName() {
+    // 페이지 입장 시, myName 값이 있는지 없는지 확인
+    // 입력된 이름은 local stroage 저장 예정
+    const name = localStorage.getItem("username");
+    if(name === null) {
+        // 처음 입장
+        // 이름 입력 > username으로 local에 저장 
+        drawInput();
+        // > innerHTML 노출
+    } else {
+        // 이름 존재
+        // username 키값으로 저장된 이름을 innHTML로 노출
+        drawName(name)
+    }
+}
+
+function init() {
+    checkName();
+}
+
+init();
